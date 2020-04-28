9aedcdadfde59d9f7e0de20f02eaaf5b212fdc1a
Nicolas Valaise <nicolas.valaise@hotmail.fr>
2020-04-28 20:00:38 +0200

    Improve the navigation in an application

--- resources/views/app/deezer.blade.php
+++ resources/views/app/deezer.blade.php
@@ -4,6 +4,14 @@
 
 @section('asset')
 <script src="{{ mix('js/vue-calendar-heatmap.browser.js') }}"></script>
+<style type="text/css">
+.disabled {
+  pointer-events: none;
+  cursor: default;
+  opacity: 0.6;
+  color: #6c757d;
+}
+</style>
 @endsection
 
 @section('sidebar-deezer')	
@@ -25,20 +33,42 @@
 				<router-link class="nav-link" :to="{ name: 'home' }" active-class="active" exact><i class="far fa-circle nav-icon"></i>Home</router-link>
 			</li>
 			<li class="nav-item">
+				<router-link class="nav-link" :to="{ name: 'artists' }" active-class="active" exact><i class="far fa-circle nav-icon"></i>Artists</router-link>
+			</li>
+			<li class="nav-item">
 				<router-link class="nav-link" :to="{ name: 'playlists' }" active-class="active" exact><i class="far fa-circle nav-icon"></i>Playlists</router-link>
 			</li>
 			<li class="nav-item">
-				<router-link class="nav-link" :to="{ name: 'social' }" active-class="active" exact><i class="far fa-circle nav-icon"></i>Social</router-link>
+				<router-link class="nav-link" :to="{ name: 'history' }" active-class="active" exact><i class="far fa-circle nav-icon"></i>History</router-link>
 			</li>
 			<li class="nav-item">
-				<router-link class="nav-link" :to="{ name: 'history' }" active-class="active" exact><i class="far fa-circle nav-icon"></i>History</router-link>
+				<router-link class="nav-link" :to="{ name: 'social' }" active-class="active" exact><i class="far fa-circle nav-icon"></i>Social</router-link>
 			</li>
 		</ul>
 	</li>
 @stop
 
 @section('content')
-	<h3>@yield('title')</h3>
+	<h3 class="text-center">@yield('title')</h3>
+	<nav aria-label="breadcrumb">
+		<ol class="breadcrumb">
+			<li class="breadcrumb-item">
+				<router-link :to="{ name: 'home' }" active-class="disabled" exact>Home</router-link>
+			</li>
+			<li class="breadcrumb-item">
+				<router-link :to="{ name: 'artists' }" active-class="disabled" exact>Artists</router-link>
+			</li>
+			<li class="breadcrumb-item">
+				<router-link :to="{ name: 'playlists' }" active-class="disabled" exact>Playlists</router-link>
+			</li>
+			<li class="breadcrumb-item">
+				<router-link :to="{ name: 'history' }" active-class="disabled" exact>History</router-link>
+			</li>
+			<li class="breadcrumb-item">
+				<router-link :to="{ name: 'social' }" active-class="disabled" exact>Social</router-link>
+			</li>
+		</ol>
+	</nav>
 
 	<router-view></router-view>
 @endsection
